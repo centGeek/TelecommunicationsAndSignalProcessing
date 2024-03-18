166aa674d655dbfd67130b7130483e393d0ea4ba
247644 Dominiak Maciej <247644@edu.p.lodz.pl>
2024-03-18 23:36:04 +0100

    Zdecydowanie nie polecam, ale w miare działa

--- main.cpp
+++ main.cpp
@@ -71,7 +71,7 @@ void initialize(char *chosenPort) {
 }
 
 //FUNKCJA ODPOWIEDZIALNA ZA ODCZYTYWANIE DANYCH Z PLIKU
-void receiveCOM(char *sign, int length) {
+void receiveCOM(unsigned char *sign, int length) {
     DWORD pos = 0, num;
     while (pos < length) {
         ReadFile(handleCom, sign + pos, length - pos, &num, NULL);
@@ -80,7 +80,7 @@ void receiveCOM(char *sign, int length) {
 }
 
 
-bool receiveCOM2(char *sign, int length) {
+bool receiveCOM2(unsigned char *sign, int length) {
     DWORD pos = 0, num;
     while (pos < length) {
         ReadFile(handleCom, sign + pos, length - pos, &num, NULL);
@@ -91,13 +91,13 @@ bool receiveCOM2(char *sign, int length) {
 
 
 //FUNKCJA ODPOWIEDZIALNA ZA ZAPISYWANIE DANYCH DO PLIKU
-void sendCOM(char *sign, int length) {
+void sendCOM(unsigned char *sign, int length) {
     DWORD num;
     WriteFile(handleCom, sign, length, &num, NULL);
 }
 
 //FUNKCJA ODPOWIEDZIALNA ZA CRC WERSJA 16 - WIELOMIAN 17 BITOWY
-short int CRC16(char *fileBuffer) {
+short int CRC16(unsigned char *fileBuffer) {
     int tmp = 0, val = 0x18005 << 15;
     for (int i = 0; i < 3; i++) {
         tmp = tmp * 256 + (unsigned char) fileBuffer[i];
@@ -120,18 +120,107 @@ short int CRC16(char *fileBuffer) {
 
 //FUNKCJA ODPOWIEDZIALNA ZA ODBIERANIE DANYCH - KORZYSTA Z FUNKCJI sendCOM() oraz receiveCOM()
 void receiving() {
-    char buf[3], fileBuffer[128];
+    unsigned char buf[3], fileBuffer[128];
     //initialize(COM);
 
-    buf[0] = crc ? C : NAK;             //WYSYLANIE ZNAKU NAK
+    buf[0] = crc ? C : NAK; //WYSYLANIE ZNAKU NAK
     sendCOM(buf, 1);
 
-    FILE *f = fopen(file, "wb"); //OTWIERANIE TRYB WB - TRYB BINARNY ORAZ NADPISYWANIE(KASUJE STARA ZAWARTOSC)
+
+    FILE *a = fopen("a.txt", "wb"); //OTWIERANIE TRYB WB - TRYB BINARNY ORAZ NADPISYWANIE(KASUJE STARA ZAWARTOSC)
+    while (true) {
+        unsigned short sum, sumc;
+        receiveCOM(buf , 3);
+        receiveCOM(fileBuffer, 128);
+
+        sum = sumc = 0;
+        receiveCOM((unsigned char *) &sum, crc ? 2 : 1);
+
+        if (crc) {
+            sumc = CRC16(fileBuffer);
+        } else {
+            for (int i = 0; i < 128; i++) {
+                sumc += (unsigned char) fileBuffer[i];
+            }
+            sumc %= 256;
+        }
+
+        printf("%x %x \n", sum, sumc);
+        if (sum != sumc) {
+            buf[0] = NAK;
+            sendCOM(buf, 1);
+            continue;
+        }
+
+        buf[0] = ACK;
+        sendCOM(buf, 1);
+
+        receiveCOM(buf, 1);
+        if (buf[0] == EOT) {
+            unsigned char last = 127;
+            while (fileBuffer[last] == SUB) {
+                last--;
+            }
+            fwrite(fileBuffer, 1, last + 1, a);
+            fclose(a);
+            buf[0]=ACK;
+            sendCOM(buf,1);
+            break;
+        }
+        fwrite(fileBuffer, 128, 1, a);
+        fclose(a);
+        buf[0]=ACK;
+        sendCOM(buf,1);
+    }
+
+
+    std::string file; // Zmienna, do której będziemy zapisywać nazwę pliku odczytaną z pliku tekstowego
+
+    // Otwórz strumień wejściowy do odczytu pliku "a.txt"
+    std::ifstream inputFile("a.txt");
+
+    // Sprawdź, czy plik został otwarty poprawnie
+    if (!inputFile.is_open()) {
+        std::cerr << "Nie można otworzyć pliku do odczytu." << std::endl;
+    }
+
+    // Odczytaj zawartość pliku i zapisz ją do zmiennej file
+    inputFile >> file;
+
+    // Zamknij plik
+    inputFile.close();
+
+
+    FILE *f = fopen(file.c_str(), "wb"); //OTWIERANIE TRYB WB - TRYB BINARNY ORAZ NADPISYWANIE(KASUJE STARA ZAWARTOSC)
 
 
     auto timeout = std::chrono::seconds(1);
 
+
     while (true) {
+        std::future<bool> resultFuture = std::async(std::launch::async, receiveCOM2, buf, 1);
+
+        std::future_status status = resultFuture.wait_for(timeout);
+
+        if (status == std::future_status::ready) {
+            // Wynik jest dostępny w określonym czasie
+            bool result = resultFuture.get();
+            if (result) {
+                std::cout << "Sukces!" << std::endl;
+                break;
+            }
+        } else if (status == std::future_status::timeout) {
+            std::cout << "Timeout!" << std::endl;
+            if (resultFuture.valid()) {
+                resultFuture.wait(); // Czekamy, aż zadanie zakończy się
+            }
+            std::cout << "Zadanie przerwane" << std::endl;
+            break;
+        }
+    }
+
+
+    /*while (true)
         sendCOM(buf, 1);
 
         cout<<"znowu?"<<endl;
@@ -152,6 +241,7 @@ void receiving() {
         }
 
     }
+    */
 
 
     // receiveCOM(buf, 1);                 //ODBIERANIE DANYCH
@@ -162,7 +252,7 @@ void receiving() {
         receiveCOM(fileBuffer, 128);
 
         sum = sumc = 0;
-        receiveCOM((char *) &sum, crc ? 2 : 1);
+        receiveCOM((unsigned char *) &sum, crc ? 2 : 1);
 
         if (crc) {
             sumc = CRC16(fileBuffer);
@@ -173,7 +263,7 @@ void receiving() {
             sumc %= 256;
         }
 
-        printf("%x %x %x\n", sum, sumc);
+        printf("%x %x \n", sum, sumc);
         if (sum != sumc) {
             buf[0] = NAK;
             sendCOM(buf, 1);
@@ -193,16 +283,33 @@ void receiving() {
             break;
         }
         fwrite(fileBuffer, 128, 1, f);
-
     }
     fclose(f);
     buf[0] = ACK;
     sendCOM(buf, 1);
 }
 
+const char* getFileName(const char* fullPath) {
+    // Konwersja do std::string dla wygody manipulacji łańcuchami znaków
+    std::string path(fullPath);
+
+    // Znajdź ostatni separator ścieżki
+    size_t pos = path.find_last_of("/\\");
+
+    // Zwróć nazwę pliku, pomijając ścieżkę
+    if (pos != std::string::npos) {
+        return path.substr(pos + 1).c_str();
+    }
+
+    // Jeśli nie ma separatorów, zwróć pełną ścieżkę
+    return fullPath;
+}
+
+
+
 //FUNKCJA ODPOWIEDZIALNA ZA ODBIERANIE DANYCH - KORZYSTA Z FUNKCJI sendCOM() oraz receivingCOM()
 void sending() {
-    char buf[3], fileBuffer[128];
+    unsigned char buf[3], fileBuffer[128];
     //initialize(COM);
 
     receiveCOM(buf, 1);
@@ -223,12 +330,88 @@ void sending() {
     // allocate memory:
 
 
+    // Otwórz strumień wyjściowy do zapisu do pliku "a.txt"
+    std::ofstream outputFile("a.txt");
+
+    // Sprawdź, czy plik został otwarty poprawnie
+    if (!outputFile.is_open()) {
+        std::cerr << "Nie można otworzyć pliku do zapisu." << std::endl;
+    }
+
+    // Zapisz nazwę pliku przechowywaną w zmiennej file do pliku tekstowego
+    outputFile << getFileName(file);
+
+    // Zamknij plik
+    outputFile.close();
+
+    std::cout << "Zapisano nazwę pliku do pliku a.txt." << std::endl;
+
+
+    FILE *a = fopen("a.txt", "rb");
+
+
+    fseek(a, 0, SEEK_END);
+
+    int fsize = ftell(a);
+
+
+    fseek(a, 0, SEEK_SET);
+    while (ftell(a) < fsize) {
+        unsigned char length = fread(fileBuffer, 1, 128, a);
+        for (int i = length; i < 128; i++) {
+            fileBuffer[i] = SUB;
+        }
+        unsigned short sum = 0;
+
+        sum = 0;
+
+        if (crc) {
+            sum = CRC16(fileBuffer);
+        } else {
+            for (int i = 0; i < 128; i++)
+                sum += (unsigned char) fileBuffer[i];
+            sum %= 256;
+        }
+
+        buf[0] = SOH;
+        buf[1] = no;
+        buf[2] = 255 - no;
+
+        sendCOM(buf, 3);
+        sendCOM(fileBuffer, 128);
+        sendCOM((unsigned char *) &sum, crc ? 2 : 1);
+
+        receiveCOM(buf, 1);
+        if (buf[0] == ACK) {
+            no++;
+        } else {
+            fseek(a, -128, SEEK_CUR);
+            ftell(a);
+        }
+    }
+
+    fclose(a);
+    do {
+        buf[0] = EOT;
+        sendCOM(buf, 1);
+        receiveCOM(buf, 1);
+    } while (buf[0] != ACK);
+
+
+    no = 1;
+
+
+    //is.open ("C:\\Final.gif", ios::binary );
+    // get length of file:
+    // allocate memory:
+
+
     FILE *f = fopen(file, "rb");
 
 
     fseek(f, 0, SEEK_END);
 
-    int fsize = ftell(f);
+    fsize = ftell(f);
 
 
     fseek(f, 0, SEEK_SET);
@@ -255,7 +438,7 @@ void sending() {
 
         sendCOM(buf, 3);
         sendCOM(fileBuffer, 128);
-        sendCOM((char *) &sum, crc ? 2 : 1);
+        sendCOM((unsigned char *) &sum, crc ? 2 : 1);
 
         receiveCOM(buf, 1);
         if (buf[0] == ACK) {
@@ -295,30 +478,11 @@ std::string openFileDialog() {
 
 int main() {
     std::string filePath;
-    cout << "Autorzy:\nKamil Kowalewski 216806\nJakub Plich 216866\n";
+    cout << "Lukasz Centkowski 247638\nMaciej Dominiak 247644\n";
 
     int choice;
     ///////////////// WYBOR TRYBU ////////////////////////
     do {
-        ifstream in_file("a.txt", ios::binary);
-        in_file.seekg(0, ios::end);
-        int file_size = in_file.tellg();
-        cout << "Size of the file is" << " " << file_size << " " << "bytes";
-        in_file.seekg(0, ios::beg);
-        file_size = in_file.tellg();
-        cout << "Size of the file is" << " " << file_size << " " << "bytes";
-        in_file.close();
-
-
-        file = openFileDialog().c_str();
-        filePath = file;
-        if (!filePath.empty()) {
-            std::cout << "Wybrany plik: " << filePath << std::endl;
-        } else {
-            std::cout << "Nie wybrano żadnego pliku." << std::endl;
-        }
-
-
         cout << "Wybierz tryb pracy:"
                 "\n1. Wysylanie"
                 "\n2. Odbieranie"
@@ -341,7 +505,8 @@ int main() {
                 << "Wybor: ";
 
         isCorrect(portNumber);
-    } while (!(portNumber == 1 || portNumber == 2 || portNumber == 3 || portNumber == 4));
+    } while (!(portNumber == 1 || portNumber == 2 || portNumber == 3 || portNumber == 4 || portNumber == 5 || portNumber
+               == 6 || portNumber == 7 || portNumber == 8));
 
     switch (portNumber) {
         case 1: {
@@ -357,6 +522,22 @@ int main() {
             break;
         }
         case 4: {
+            COM = "COM4";
+            break;
+        }
+        case 5: {
+            COM = "COM1";
+            break;
+        }
+        case 6: {
+            COM = "COM2";
+            break;
+        }
+        case 7: {
+            COM = "COM3";
+            break;
+        }
+        case 8: {
             COM = "COM4";
             break;
         }
@@ -381,6 +562,15 @@ int main() {
     ///////////// URUCHAMIANIE DZIALANIA /////////////
     switch (choice) {
         case 1: {
+            string a = openFileDialog();
+            cout<<a;
+            file = a.c_str();
+            filePath = file;
+            if (!filePath.empty()) {
+                std::cout << "Wybrany plik: " << filePath << std::endl;
+            } else {
+                std::cout << "Nie wybrano żadnego pliku." << std::endl;
+            }
             sending();
             break;
         }
